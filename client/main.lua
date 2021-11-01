d1d1c9ab6df7653387a4d4a601b534099fd99986
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-01 21:17:47 +0200

    GetHashKey replace

--- client/main.lua
+++ client/main.lua
@@ -113,7 +113,7 @@ RegisterNetEvent("weapons:client:EquipAttachment", function(ItemData, attachment
     local ped = PlayerPedId()
     local weapon = GetSelectedPedWeapon(ped)
     local WeaponData = QBCore.Shared.Weapons[weapon]
-    if weapon ~= GetHashKey("WEAPON_UNARMED") then
+    if weapon ~= `weapon_unarmed` then
         WeaponData.name = WeaponData.name:upper()
         if WeaponAttachments[WeaponData.name] then
             if WeaponAttachments[WeaponData.name][attachment]['item'] == ItemData.name then
