d0b817ad2b86cc798d40f218c165ed7965c91e8a
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-05 19:12:05 +0200

    capital letter

--- client/main.lua
+++ client/main.lua
@@ -113,7 +113,7 @@ RegisterNetEvent("weapons:client:EquipAttachment", function(ItemData, attachment
     local ped = PlayerPedId()
     local weapon = GetSelectedPedWeapon(ped)
     local WeaponData = QBCore.Shared.Weapons[weapon]
-    if weapon ~= `weapon_unarmed` then
+    if weapon ~= `WEAPON_UNARMED` then
         WeaponData.name = WeaponData.name:upper()
         if WeaponAttachments[WeaponData.name] then
             if WeaponAttachments[WeaponData.name][attachment]['item'] == ItemData.name then
