980b22723bf8d47ea1ba0ad0587e3a70997a1802
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-11-10 01:04:44 -0600

    lua54

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,12 @@ game 'gta5'
 description 'QB-Weapons'
 version '1.0.0'
 
-shared_scripts { 
-	'@qb-core/import.lua',
-	'config.lua'
-}
-
+shared_script 'config.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
 
-files {
-    'weaponsnspistol.meta',
-}
+files {'weaponsnspistol.meta'}
 
 data_file 'WEAPONINFO_FILE_PATCH' 'weaponsnspistol.meta'
+
+lua54 'yes'
