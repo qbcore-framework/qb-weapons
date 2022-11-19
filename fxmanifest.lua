ae657c2b72f62673413b434dc78643a3aebd0115
Charlie <dev@charlie.fyi>
2022-11-19 12:03:49 +0000

    Bump Version / Tidy

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,20 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Weapons'
-version '1.0.0'
+version '1.2.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua',
-	'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'config.lua',
 }
 
 server_script 'server/main.lua'
 client_script 'client/main.lua'
 
-files {'weaponsnspistol.meta'}
+files {
+    'weaponsnspistol.meta'
+}
 
 data_file 'WEAPONINFO_FILE_PATCH' 'weaponsnspistol.meta'
 
