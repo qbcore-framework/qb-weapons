537feeac062e72ec54eeee00175c0103971acacb
Luke <62490330+Luke20201@users.noreply.github.com>
2024-11-17 15:53:40 +0000

    Config.ammotypes

--- config.lua
+++ config.lua
@@ -9,6 +9,16 @@ Config.DurabilityBlockedWeapons = {
     'weapon_unarmed',
 }
 
+Config.AmmoTypes = {
+    pistol_ammo = { ammoType = 'AMMO_PISTOL', amount = 30 },
+    rifle_ammo = { ammoType = 'AMMO_RIFLE', amount = 30 },
+    smg_ammo = { ammoType = 'AMMO_SMG', amount = 30 },
+    shotgun_ammo = { ammoType = 'AMMO_SHOTGUN', amount = 10 },
+    mg_ammo = { ammoType = 'AMMO_MG', amount = 30 },
+    snp_ammo = { ammoType = 'AMMO_SNIPER', amount = 10 },
+    emp_ammo = { ammoType = 'AMMO_EMPLAUNCHER', amount = 10 },
+}
+
 Config.Throwables = {
     'ball',
     'bzgas',
